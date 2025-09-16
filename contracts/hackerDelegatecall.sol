7189e59cba2a72c9a2559587657bd6ce0c89c44c
ZWJ <34937436@qq.com>
2025-09-16 14:28:48 +0800

    delegatecall黑客钓鱼和调用者msg.sender

--- contracts/hackerDelegatecall.sol
+++ contracts/hackerDelegatecall.sol
@@ -53,6 +53,6 @@ contract HackerMalice {
 
 //总结：
 //1. delegatecall 相当于把调用合约的函数执行在当前上下文，如果涉及到修改变量一定要注意顺序定义一致性
-//2. delegatecall 的msg.sender指向的是调用者，而不是穿透到最原始调用者
+//2. delegatecall 的msg.sender指向的是使用delegatecall当前上下文的msg.sender调用者保持msg.sender不变，而不是穿透到最原始调用者
 
 //delegatecall  能实现黑客攻击，只要有人调用了钓鱼合约，那么就能拿到msg.sender调用人权限（如果是eoa可以转走代币，如果是合约账户可以转走eth）
